9dd17351c581b0208765c493bf458892b2a7b840
haseeb <chaudhuryhaseeb35@gmail.com>
2023-08-25 21:45:43 -0400

    vercel error

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -13,11 +13,12 @@ const About = () => {
           </p>
           <h2 className='py-4'>Who I Am</h2>
           <p className='py-2 text-gray-600'>
-          My name is Haseeb Chaudhury, and I am currently a computer science student at the City College of New York in my senior year. During my second year of college, I embarked on a journey into web development, and that's when I discovered my true passion. I thoroughly enjoyed crafting websites and exploring various technologies to develop software solutions
+         {`My name is Haseeb Chaudhury, and I am currently a computer science student at the City College of New York in my senior year. During my second year of college, I embarked on a journey into web development, and that's when I discovered my true passion. I thoroughly enjoyed crafting websites and exploring various technologies to develop software solutions`} 
           </p>
           <p className='py-2 text-gray-600'>
-          I am passionate about learning new technologies and believe in the importance of understanding multiple approaches to problem-solving. While I am most proficient in building front-end applications using HTML, CSS, JavaScript, and React, I approach new tech stacks with a quick learning curve, allowing me to adapt to the demands of diverse projects. I believe that being a great developer is not using one specific language, but choosing the best tool for the job.
-          When I am not coding, I find enjoyment in playing video games, engaging in chess matches, and immersing myself with mangas."
+            {`  I am passionate about learning new technologies and believe in the importance of understanding multiple approaches to problem-solving. While I am most proficient in building front-end applications using HTML, CSS, JavaScript, and React, I approach new tech stacks with a quick learning curve, allowing me to adapt to the demands of diverse projects. I believe that being a great developer is not using one specific language, but choosing the best tool for the job.
+          When I am not coding, I find enjoyment in playing video games, engaging in chess matches, and immersing myself with mangas.`}
+        
           </p>
           <Link href='/#projects'>
             <p className='py-2 text-gray-600 underline cursor-pointer'>
